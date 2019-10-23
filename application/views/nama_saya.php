6d4b0d1b4ff6684d088e6dd58e4bea1ab79576e0
rosifulaqli <rosifulaqli007@gmail.com>
2019-10-23 12:26:48 +0700

    tugas praktikum ci

---
+++ application/views/nama_saya.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<H1> praktikum</H1>
+
+</body>
+</html>
